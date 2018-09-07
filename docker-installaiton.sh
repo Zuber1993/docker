357522e2eaf03b95bafff521e6867de32241216f
juber sheikh <juber.sheikh@reactiveworks.in>
2018-09-07 13:03:35 +0530

    Update docker-installaiton.sh

--- docker-installaiton.sh
+++ docker-installaiton.sh
@@ -1,32 +1,5 @@
 #!/bin/bash
 
-which docker
 
-if [ $? -eq 0 ]
-then
-    docker --version | grep "Docker version"
-    if [ $? -eq 0 ]
-    then
-        echo "docker already  existing"
-    else
-        echo "install docker"
-    fi
-else
-
-read  -p "Install docker service (y)?" docker
-
-if [ $docker = y ]
-  then
   curl -sSL https://get.docker.com/ | sh
 
-#else
-    #echo "docker is already install"
-
-fi
-
-fi
-
-
-# $? = was last command successful. Answer is 0 which means 'yes'.
-# $# = number of arguments. Answer is 3.
-# $@ = what parameters were passed. Answer is 1 2 3.
