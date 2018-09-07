33e41c0a25e64b31392954ece71d71750344fc3f
juber sheikh <juber.sheikh@reactiveworks.in>
2018-09-07 13:12:42 +0530

    Update docker-installaiton.sh

--- docker-installaiton.sh
+++ docker-installaiton.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+which docker
 
-  #curl -sSL https://get.docker.com/ | sh
+if [ $? -eq 0 ]
+then
+    docker --version | grep "Docker version"
+    if [ $? -eq 0 ]
+    then
+        echo "docker already  existing"
+    else
+        echo "install docker"
+    fi
+else
 
-apt-get update
-apt-get install nginx -x
+  curl -sSL https://get.docker.com/ | sh
